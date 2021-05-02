047ef524e2bcc6ada98f7306560b7c86abcdce8a
Roody <rjointer2@gmail.com>
2021-05-02 08:40:28 -0700

    push to production

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ShopContainer from './appComponents/ShopContainer';
+
+function App() {
+  return (
+    <div className="App">
+        <ShopContainer/>
+    </div>
+  );
+}
+
+export default App;
